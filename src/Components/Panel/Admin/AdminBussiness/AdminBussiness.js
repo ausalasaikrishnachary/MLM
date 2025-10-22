862b76997469cdc81734765fd2e05a0f591e6282
srithaalwar <srithaalwar4@gmail.com>
2025-10-22 16:05:44 +0530

    sritha

--- src/Components/Panel/Admin/AdminBussiness/AdminBussiness.js
+++ src/Components/Panel/Admin/AdminBussiness/AdminBussiness.js
@@ -449,21 +449,19 @@ function ViewBusiness() {
                 <Grid item xs={12} sm={6} md={4} key={business.business_id}>
                   <Card
                     sx={{
-                      borderRadius: 3,
-                      boxShadow: 4,
-                      height: "100%",
+                   borderRadius: "20px",
+                      p: 2,
+                      boxShadow: "0 6px 18px rgba(0,0,0,0.08)",
+                      bgcolor: "#fff",
                       display: "flex",
                       flexDirection: "column",
+                      justifyContent: "space-between",
+                      width: "100%",
+                      height: "100%",
                       position: "relative",
-                      overflow: "hidden",
-                      borderTopLeftRadius: "15px",
-                      borderTopRightRadius: "15px",
+                      overflow: "visible",
                       cursor: 'pointer',
-                      '&:hover': {
-                        transform: 'translateY(-5px)',
-                        boxShadow: '0 4px 15px rgba(0,0,0,0.749)',
 
-                      }
                     }}
                     onClick={() => navigate(`/a-businessproducts/${business.business_id}`)}
                   >
